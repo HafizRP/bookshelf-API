640d5b3ccb9ccf8125991e925463b1095964e1b0
PKL TOHO <you@example.com>
2022-08-10 10:39:49 +0700

    add delete and update books

--- index.js
+++ index.js
@@ -1,43 +1,125 @@
-import express from "express"
-import fs from 'fs'
-import { body, check, validationResult } from 'express-validator'
-import { nanoid } from 'nanoid'
+import express from "express";
+import fs, { read } from "fs";
+import { body, check, validationResult } from "express-validator";
+import { nanoid } from "nanoid";
 
-const db = JSON.parse(fs.readFileSync('./books.json', 'utf-8'))
-const app = express()
+const db = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
+app.get("/books", async (req, res) => {
     try {
-        res.send({ status: "success", data: db })
+        res.send({ status: "success", data: db });
     } catch (error) {
-        res.status(400).send({ status: "failed", data: [] })
+        res.status(400).send({ status: "failed", data: "Server error" });
     }
+});
+
+app.get('/books/:bookId', (req, res) => {
+    const data = db.find(val => val.id === req.params.bookId)
+
+    if (!data) return res.status(404).send({ status: "failed", message: "book not found" })
+    else res.send({
+        status: 'success', data: {
+            book: data
+        }
+    })
 })
 
-app.post('/books', body('name').custom(value => {
-    if (value === null || value === '') return Promise.reject("name has to defined!")
-    else return Promise.resolve()
-}), async (req, res) => {
+
+function checkNewBooks(req, res, next) {
+    if (req.body.name === null || req.body.name === '') return res.status(400).send({ status: "failed", data: "name has to defined!" })
+    if (req.body.readPage > req.body.pageCount) return res.status(400).send({ status: "failed", data: "Read page has to lower than Page Count!" })
+    next()
+}
+
+app.post(
+    "/books",
+    checkNewBooks,
+    async (req, res) => {
+        const {
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            reading,
+        } = req.body;
+        try {
+            const data = {
+                id: nanoid(),
+                name: name || null,
+                year: year || null,
+                author: author || null,
+                summary: summary || null,
+                publisher: publisher || null,
+                pageCount: pageCount || null,
+                readPage: readPage || 0,
+                reading: reading || false,
+                finished: pageCount === readPage,
+                insertedAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString()
+            };
+
+            db.push(data);
+            fs.writeFileSync("books.json", JSON.stringify(db, null, 4));
+
+            res.status(201).send({ status: "success", data: { bookId: data.id } });
+        } catch (error) {
+            res.status(500).send({ status: "failed", reason: "Server error" });
+        }
+    }
+);
+
+app.put('/books/:bookId', checkNewBooks, async (req, res) => {
+    const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading,
+    } = req.body;
     try {
-        const errors = validationResult(req)
+        const data = db.find(val => val.id === req.params.bookId)
 
-        if (!errors.isEmpty()) return res.status(400).send({ status: "failed", data: errors })
-        const { name, year, author, summary, publisher, pageCount, readPage, reading } = req.body
-        const data = { id: nanoid(), name: name, year: year, author: author || null, summary: summary, publisher: publisher, pageCount: pageCount, readPage: readPage, reading: reading }
+        if (!data) return res.status(404).send({ status: 'fail', message: 'book not found' })
 
-        db.push(data)
+        Object.assign(data, { name: name, year: year, author: author, summary: summary, publisher: publisher, pageCount: pageCount, readPage: readPage, reading: reading, updatedAt: new Date().toISOString() })
 
         fs.writeFileSync('books.json', JSON.stringify(db, null, 4))
-        res.status(201).send({ status: "success" })
+
+        res.status(200).send({ status: "success", message: "book edit successfull" })
     } catch (error) {
-        res.status(500).send({ status: 'failed', reason: "Server error" })
+        res.status(500).send({ status: "failed", message: "server error" })
     }
 })
 
+app.delete('/books/:bookId', (req, res) => {
+    try {
+        const data = db.findIndex(val => val.id === req.params.bookId)
+
+        if (data === -1) return res.status(404).send({ status: "failed", message: "book not found" })
+
+        db.splice(data, 1)
+
+        fs.writeFileSync('books.json', JSON.stringify(db, null, 4))
+
+        res.status(200).send({ status: 'success', message: "book successfullt deleted!" })
+    } catch (error) {
+        res.status(500).send({ status: "failed", message: "server error" })
+    }
+
+
+
+})
 
 app.listen(5000, () => {
-    console.log("Server was open in port 5000!")
-})
+    console.log("Server was open in port 5000!");
+});
